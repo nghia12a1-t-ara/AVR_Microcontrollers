b5b5037e76dad7589df6ccf99bac3b000648fc59
“Nghia12a1” <nguyenvannghia2914@gmail.com>
2024-07-23 21:42:02 +0700

    First Commit for AVR Repo

---
+++ 1_AVR_Basic_Peripheral/GPIO_LedBlink/GPIO_LedBlink/main.c
@@ -0,0 +1,26 @@
+/*
+ * GPIO_LedBlink.c
+ *
+ * Created: 7/17/2024 9:08:15 PM
+ * Author : Nghia Taarabt
+ */ 
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRA |= (1U << 0);				/* Making Pin A0 as output pins */
+	
+    while(1)
+    {
+        PORTA |= (1U << 0);			/* Making PA0 high. This will make LED ON */
+        _delay_ms(100);
+		PORTA &= ~(1U << 0);		/* Making PA0 low. This will make LED OFF */
+        _delay_ms(100);
+    }
+	return 0;
+}
+
+
